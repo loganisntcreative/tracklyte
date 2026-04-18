07973148367d5f660f9273781e3e3f577141cc1a
loganisntcreative <loganvenord123@gmail.com>
2026-04-18 16:49:06 -0700

    Fix session config

--- run.py
+++ run.py
@@ -4,6 +4,9 @@ load_dotenv()
 from app import create_app
 
 app = create_app()
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = 86400  # 1 day
 
 if __name__ == '__main__':
     app.run(debug=False)
