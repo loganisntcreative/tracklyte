c0569b37ab6189b12eb97c78eaaf50ff9b6abe49
loganisntcreative <loganvenord123@gmail.com>
2026-04-18 16:33:29 -0700

    Fix verification email URL for production

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ WTF_CSRF_ENABLED = True
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-change-this-later'
     _db_url = os.environ.get('DATABASE_URL') or 'sqlite:///tracklyte.db'
-    # Render gives postgres:// but SQLAlchemy needs postgresql://
     SQLALCHEMY_DATABASE_URI = _db_url.replace('postgres://', 'postgresql://', 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CACHE_TYPE = 'SimpleCache'
     CACHE_DEFAULT_TIMEOUT = 300
+    SERVER_NAME = os.environ.get('SERVER_NAME')
